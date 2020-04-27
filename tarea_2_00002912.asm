6a863be29ed079833d1927ed724cd145ef6164cc
wescalante <54720501+wescalante@users.noreply.github.com>
2020-04-26 23:15:50 -0500

    Add: Ejercicio 3
    Add: Ejercicio 3

--- tarea_2_00002912.asm
+++ tarea_2_00002912.asm
@@ -45,17 +45,39 @@ mov     al, 2d
 mov     bx, 210h
 mov     cx, 2d
 
-mult:mul cx
+mult:	mul cx
 
 mov     [bx], ax
 cmp     ah, 00h
 ja      segu
 je      prim
-segu:add bx, 2h
+segu:	add bx, 2h
 jmp     sig
-prim:add bx, 1h
-sig:cmp bx, 21Fh
+prim:	add bx, 1h
+sig:	cmp bx, 21Fh
 jb      mult
+
+;Ejercicio 3
+
+mov     ax, 0000h
+mov     ax, 0d
+mov     [220h], ax
+mov     ax, 1d
+mov     [221h], ax  ; F1 = 1
+mov     bx, 222h
+
+fib:	mov     ax, 0000h
+
+mov     [bx], ax
+sub     bx, 2h
+mov     al, [bx]
+add     bx, 1h
+add     ax, [bx]
+add     bx, 1h
+mov     [bx], ax
+add     bx, 1h
+cmp     bx, 22Fh
+jb      fib
 
 int 20h
 
